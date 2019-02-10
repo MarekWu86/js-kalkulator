ec4a12f46f317965bf16db90d904e6bc2914e46c
MarekWu86 <marek.wojdylo@m3group.pl>
2019-02-10 22:16:59 +0100

    działa

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-    var number1;
-    var number2;
-
-function getResult() {
+function getResult() { 
     var number1 = prompt("Proszę wpisz liczbę x", "x");
     var number2;
-    if (number1 != null) {
+    if (number1 !== null) {
       number2 = prompt("Podaj liczbę y", "y");
     }
-    document.getElementById("wynik").innerHTML = "Liczba 1: " + number1 + ".<br>Liczba 2: " + number2 + ".";
+    document.getElementById("liczba1").innerHTML = "Liczba1: " + number1;
+    document.getElementById("liczba2").innerHTML = "Liczba2: " + number2;
   }  
 
 function getAdded() {
-    var number3 = number1 + number2;
+  var number1 = parseInt(document.getElementById("liczba1").innerHTML.split(":")[1]);
+  var number2 = parseInt(document.getElementById("liczba2").innerHTML.split(":")[1]);
+  var number3 = number1 + number2;
     document.getElementById("wyniki").innerHTML = number3;
 }
 
 
 function getDivided() {
-    var number4 = number1 - number2;
+  var number1 = parseInt(document.getElementById("liczba1").innerHTML.split(":")[1]);
+  var number2 = parseInt(document.getElementById("liczba2").innerHTML.split(":")[1]);
+  var number4 = number1 - number2;
     document.getElementById("wyniki").innerHTML = number4;
 }
 
 function getMultiplied() {
-    var number5 = number1 * number2;
+  var number1 = parseInt(document.getElementById("liczba1").innerHTML.split(":")[1]);
+  var number2 = parseInt(document.getElementById("liczba2").innerHTML.split(":")[1]);
+  var number5 = number1 * number2;
     document.getElementById("wyniki").innerHTML = number5;
 }
